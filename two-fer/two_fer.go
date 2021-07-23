6cc091702f6928e54663741739c064b3862e535d
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2021-07-23 00:01:04 -0300

    reafactor: remove dependecie and add comments

--- two-fer/two_fer.go
+++ two-fer/two_fer.go
@@ -1,10 +1,10 @@
+// Package twofer provide one function for resolve an exercism
 package twofer
 
-import "fmt"
-
+// ShareWith share with the name param and me. If name not provided put 'you'.
 func ShareWith(name string) string {
 	if name == "" {
 		name = "you"
 	}
-	return fmt.Sprintf("One for %s, one for me.", name)
+	return "One for " + name + ", one for me."
 }
